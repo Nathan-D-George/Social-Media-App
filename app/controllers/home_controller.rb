6ef77af62bea3d2dc02ee17fc666266b1629dd2b
Nathan-D-George <nathan.intern@bitcube.tech>
2023-06-13 11:20:16 +0200

    set up basics (home, about, navbar, bootstrap, flashes, fav_icons)

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+    def home
+    end
+    def about
+    end
+    def secret
+        flash.now[:notice] = 'Well done on reaching the real secret room'
+    end
+    def fakesecret
+        flash.now[:alert] = 'Not the real secret room'
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home#home'
+  get '/about',       to: 'home#about'
+  get '/fake_secret', to: 'home#fakesecret'
+  get '/secret',      to: 'home#secret'
+
+  #get '/feed', to: 'social#feed'
+  #get '/dms',  to: 'social#dms'
 end
